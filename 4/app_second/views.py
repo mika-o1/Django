46e2e3599c9216334b0d942f9196ee07217c3121
mika-o1 <96731583+mika-o1@users.noreply.github.com>
2022-05-19 04:48:39 +0600

    update obj

--- 4/app_second/views.py
+++ 4/app_second/views.py
@@ -1,55 +1,167 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-
+from multiprocessing import context
+from django.shortcuts import render
+from django.http import HttpResponse
 from . import models
 
 
 # тут только "логика" - функции для обработки и возврат данных
 
-def index(request):
-    return render(request, 'app_teacher/pages/index.html')
 
+def index(request):
+    # return HttpResponse("Это чистая индекс страница")
+    context = {
+
+    }
+    return render(request, 'app_second/pages/index.html', context)
+
+
+def func(description, name="Apple"):
+    pass
 
 def home(request):
-    return render(request, 'app_teacher/pages/home.html')
+    context = {
+
+    }
+    return render(request, 'app_second/pages/home.html', context)
+
+
+def login(request):
+    context = {
+
+    }
+    return render(request, 'app_second/pages/login.html', context)
 
 
 def about(request):
-    return render(request, 'app_teacher/pages/about.html')
+    context = {
+
+    }
+    return render(request, 'app_second/pages/about.html', context)
 
 
 def origin_home(request):
-    return render(request, 'app_teacher/pages/origin_home.html')
+    context = {
+
+    }
+    return render(request, 'app_second/pages/origin_home.html', context)
 
 
 def todo_detail(request, todo_id):
+    print(request)
+    if request.method == "GET":
+        print("это GET запрос")
+    if request.method == "POST":
+        print("это POST запрос")
+    if request.method == "PUT":
+        print("это PUT запрос")
+    if request.method == "DELETE":
+        print("это DELETE запрос")
+
+    # print("request.method: ", request.method)
+    # print("request.path: ", request.path)
+    # print("request.headers: ", request.headers)
+    # print("request.META: ", )
+
+    # пробегаемся по ключам словаря
+    # for key, value in request.META.keys():
+
+    # пробегаемся по значениям словаря
+    # for key, value in request.META.values():
+
+    # пробегаемся по парам: ключ-значение
+    # for key, value in request.META.items():
+    #     print(f"{key}: {value}")
+    # print("request.data: ", request.data)
+    # print("request.GET: ", request.GET)
+
+    is_completed = False
+    if is_completed:
+        pass
+    else:
+        pass
+
+    list = [1, 2, 3]
+    for i in list:
+        index = list.index(i)
+        # print(i)
+        pass
+
     obj = models.Task.objects.get(id=todo_id)
+
     context = {
         "todo": obj
     }
-    return render(request, 'app_teacher/pages/DetailTodo.html', context)
+    return render(request, 'app_second/pages/DetailTodo.html', context)
 
 
 def todo_list(request):
+    print(request)
+    if request.method == "GET":
+        print("это GET запрос")
+    if request.method == "POST":
+        print("это POST запрос")
+    if request.method == "PUT":
+        print("это PUT запрос")
+    if request.method == "DELETE":
+        print("это DELETE запрос")
+
+
+    # print("request.method: ", request.method)
+    # print("request.path: ", request.path)
+    # print("request.headers: ", request.headers)
+    # print("request.META: ", )
+
+    # пробегаемся по ключам словаря
+    # for key, value in request.META.keys():
+
+    # пробегаемся по значениям словаря
+    # for key, value in request.META.values():
+
+    # пробегаемся по парам: ключ-значение
+    # for key, value in request.META.items():
+    #     print(f"{key}: {value}")
+    # print("request.data: ", request.data)
+    # print("request.GET: ", request.GET)
+
+    is_completed = False
+    if is_completed:
+        pass
+    else:
+        pass
+
+    list = [1, 2, 3]
+    for i in list:
+        index = list.index(i)
+        # print(i)
+        pass
+
     obj = models.Task.objects.all()
+
+    print(f"obj: {obj}")
+    print(f"obj count: {obj.count()}")  
+
     context = {"list": obj}
-    return render(request, 'app_teacher/pages/todo_list.html', context)
+
+    return render(request, 'app_second/pages/todo_list.html', context)
 
 
 def todo_create(request):
+    print("todo_create")
     if request.method == "POST":
+        print("это POST запрос")
+
+        # вызывается Exception (исключение)
+        # title = request.POST["title"]
         title1 = request.POST.get("title", "заголовок по умолчанию")
         description1 = request.POST.get("description", "описание по умолчанию")
+
         obj = models.Task.objects.create(
             title=title1,
             description=description1
         )
         obj.save()
+
+
+        # приём и обработка данных
     context = {}
-    return render(request, 'app_teacher/pages/CreateTodo.html', context)
-
-
-def todo_delete(request, todo_id):
-    obj = models.Task.objects.get(id=todo_id)
-    obj.delete()
-    return redirect(reverse('todo_list', args=()))
+    return render(request, 'app_second/pages/CreateTodo.html', context)
