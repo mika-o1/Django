7dd8fad5ff08d5e78d548bb309166d097f3d2a09
mika-o1 <96731583+mika-o1@users.noreply.github.com>
2022-05-25 02:21:23 +0600

    update

--- 5/app_student/urls.py
+++ 5/app_student/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
+from . import views as logic
+
+# тут только "маршруты" - адрес страницы
 urlpatterns = [
+    path(route='my_todo_list/', view=logic.my_todo_list, name="my_todo_list"),
+
 ]
--- 5/app_student/views.py
+++ 5/app_student/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from app_teacher import models
 
-# Create your views here.
+
+def my_todo_list(request):
+    obj = models.Task.objects.all()
+    context = {"list": obj}
+    return render(request, 'app_student/pages/my_todo_list.html', context)
+
+
